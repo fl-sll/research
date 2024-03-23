e241b440ee99074d0ad0e2e84c7922bba4eedb44
fl-sll <edwardalvin@gmail.com>
2024-03-23 14:30:37 +0700

    transfer data to mongodb script

--- data.py
+++ data.py
@@ -1,7 +1,22 @@
 import pymongo
 
-client = pymongo.MongoClient("mongodb://localhost:27017/")  
-db = client["chatbot"]
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://edwardalvin:mBDH6fh9bUjCDcA2@reseearch.a2rwr6l.mongodb.net/?retryWrites=true&w=majority&appName=reseearch"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+
+db = client["research"]
 collection = db["data"] 
 
 intents_data =  [
